63ca686435ecba0700c2051aa40714b737fd297b
Stefania Giannina Arias Farias <stefaniaarias@Stefanias-MacBook-Pro.local>
2024-02-20 23:22:21 +0100

    Creating header container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import { SearchBar } from '../SearchBar/SearchBar';
 import './Header.scss';
+import meLiLogo from '../../assets/images/MeLiLogo.png';
 
 export function Header(){
     return (
         <div className='header'>
+            <div className='header_logo'>
+                <img src={meLiLogo} height={25} alt="MeLi logo" />
+            </div>
             <SearchBar/>
         </div>
     );
