d09aa7713c62c1460338c5a85e1cc30deeb33629
Stefania Giannina Arias Farias <stefaniaarias@Stefanias-MacBook-Pro.local>
2024-02-27 16:58:03 -0300

    Adding login in searchbar when user is no auth

--- react/src/components/MainContainer/MainContainer.tsx
+++ react/src/components/MainContainer/MainContainer.tsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Header } from '../Header/Header';
 import './MainContainer.scss';
 
 export function MainContainer () {
+    const [isAuth, setIsAuth] = useState(false);
+    
+    useEffect(() => {
+        if (localStorage.getItem('access_token') !== null) {
+            setIsAuth(true); 
+        }
+        
+    }, [isAuth]);
+
     return (
-    <div className='main-container'> 
-        <Header/>
-    </div>
+        <div className='main-container'> 
+            <Header isAuth={isAuth}/>
+        </div>
     );
 }
